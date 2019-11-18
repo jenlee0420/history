d34405c2e920babd787ba387d45c35ab6b8756b0
liqin <liqin@tooklink.cn>
2019-11-18 18:52:45 +0800

    。。

--- chist_s1/js/main.js
+++ chist_s1/js/main.js
@@ -298,11 +298,9 @@ $(function() {
         bind_sound($("#soundCon"))
         $("#loading").hide();
         $("#main_container").show();
+        $('.ui-button').html('<span class="ui-icon-closethick"></span>')
         setPop()
-        $(".closeBtn2").on('click', function () {
-            mp4_container.dialog('close')
-            // mp4div.dialog("option", "title", title).dialog('open');
-        })
+        
         // if(/Android/.test(u_agent)){
         //     $(".ui-slider-horizontal .ui-slider-handle").css('top','-35%')
         // }
