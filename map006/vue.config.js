fdd7860a8db001131a4d28eb4c471b088867969c
liqin <liqin@tooklink.cn>
2019-12-03 15:28:21 +0800

    init map006

---
+++ map006/vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    publicPath: "./",
+    outputDir: "dist",
+    assetsDir: "assets",
+    indexPath: "index.html",
+    filenameHashing: true,
+    pages: undefined,
+    lintOnSave: true,
+    runtimeCompiler: false,
+    transpileDependencies: [],
+    productionSourceMap: false,
+    crossorigin: undefined,
+    integrity: false,
+    devServer: {//代理
+        disableHostCheck: false,
+        host: "0.0.0.0",
+        port: 8080,
+        proxy: null
+    },
+    lintOnSave: false,
+
+
+}
