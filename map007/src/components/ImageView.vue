c97509b07f7e4801780a1fd3b09e3253056ef109
jenlee0420 <jenlee0420>
2020-03-07 23:01:33 +0800

    优化007的动画卡顿问题

---
+++ map007/src/components/ImageView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="pos_a map" v-show="imgStatic" :style="{'z-index':zindex}">
+        <img :src="require(`../../static/img/${this.imgsrc}`)" alt="" @load="load">
+    </div>
+</template>
+<script>
+export default {
+  name: "imageview",
+  props: [
+      "imgsrc",
+      "static",
+      "zindex"
+  ],
+  mounted(){
+
+  },
+  computed:{
+      imgStatic:function(){
+          return this.static && this.imgReady
+      }
+  },
+  data() {
+      return{
+          imgObj:null,
+          imgReady:false
+      }
+  },
+   methods: {
+       load(){
+           this.imgReady= true
+       }
+   }
+}
+</script>
+<style lang="less">
+.map img{
+    width: 100%;
+    height: 100%;
+}
+</style>
