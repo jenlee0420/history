7146204f40cd8996f870039974f84e0262d17bf2
jenlee0420 <jenlee0420>
2020-10-13 10:40:27 +0800

    init 003

---
+++ chist_s1_map_003/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../static/css/default.css'
+import global from './js/animate';
+
+Vue.config.productionTip = false
+/* 全局变量，方法 */
+Object.keys(global).forEach(key => {
+  Vue.prototype[key] = global[key];
+});
+import Vconsole from 'vconsole';
+
+let vConsole = new Vconsole();
+
+export default vConsole
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
